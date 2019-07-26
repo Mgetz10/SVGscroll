4df6d05f97ed6c58caca58d35cfd0e46b14f00ab
Monroe <monroe.getz@simpleshow.com>
2019-07-26 17:49:11 -0400

    dNd bugfix

--- project/javascript/script.js
+++ project/javascript/script.js
@@ -4,9 +4,13 @@ const bottomContainer = document.querySelector('.bottomContainer')
 const interactionIframe = document.querySelector('.interactionIframe')
 
 function iframeShow() {
-    optionsContainer.style.top = '74.5%'
-    bottomContainer.style.top = '85.8%'
     interactionIframe.classList.remove('hidden')
 }
+
+function hideIframe() {
+    interactionIframe.classList.add('hidden')
+}
+
+interactionIframe.addEventListener('click', hideIframe)
 
 interactiveBtns.forEach(btn => btn.addEventListener('click', iframeShow))
